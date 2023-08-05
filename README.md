1c3fc644c0bf213df5e2eae31b44dd775387d308
Satya Deep Maheshwari <satyam@adobe.com>
2023-08-05 13:44:14 +0530

    Franklin bulk actions

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # web-tools
 A collection of handy tools to aid website development.
+
+## LSR Bookmarklet
+```
+javascript: (() => {
+    const origin = window.prompt('Origin');
+    const omitFolders = window.prompt('Comma seperated list of folders to omit');
+    const action = window.prompt('For Preview type 1 and for Publish type 2');
+    if (origin && action) {
+        window.domainPrefix = origin;
+        window.omitFolders = omitFolders;
+        window.action = action;
+        const script = document.createElement('script');
+        script.src = 'http://18.207.211.159:3000/lsr.js';
+        document.head.append(script);
+    }
+})();
+```
---
+++ public/lsr.js
@@ -0,0 +1,106 @@
+const frkActions = [ 'preview', 'live'];
+let count = 0;
+const MAX_COUNT = 1000;
+
+function parseURL(url) {
+  const regex = /^https:\/\/([\w-]+)--([\w-]+)--([\w-]+)\.hlx\.page\/([\w\/-]+(\.\w+)?)$/;
+  const matches = url.match(regex);
+
+  if (matches) {
+    const [, branch, repo, org, path, extension] = matches;
+    const result = {
+      branch: branch,
+      repo: repo,
+      org: org,
+      path: path,
+    };
+    return result;
+  } else {
+    return null;
+  }
+}
+
+
+async function postData(url, action) {
+  const json = parseURL(url);
+  const postUrl = `https://admin.hlx.page/${frkActions[action - 1]}/${json.org}/${json.repo}/${json.branch}/${json.path}`;
+
+  try {
+    console.log('Posting to', postUrl);
+    const response = await fetch(postUrl, { method: 'POST' });
+    console.log('Response:', response.status);
+  } catch (error) {
+    // Handle any errors that occurred during the request
+    console.error('Error:', error);
+  }
+}
+
+async function traverseCurrentFolder() {
+  const URLs = [];
+  const [prefix] = window.location.href.split('/Shared%20Documents/');
+  const usp = new URLSearchParams(window.location.search);
+  const rootPath = usp.get('id').split('/Shared Documents')[1];
+  const omittedFolders = window.omitFolders.split(',').map(item => item.trim()) || [];
+  omittedFolders.push('import-report.xlsx');
+  omittedFolders.push('query-index.xlsx');
+  const action = window.action;
+  const domainPrefix = window.domainPrefix.endsWith('/') ? window.domainPrefix.substring(0, window.domainPrefix.length - 1) : window.domainPrefix;
+
+  const traverseFolder = async (path) => {
+    if(count > 10) return;
+    for (let i = 0; i < omittedFolders.length; i += 1) {
+      if (path.endsWith(omittedFolders[i])) {
+        console.log(`Skipping ${path}`);
+        return;
+      }
+    }
+    const getDirEntries = async (path, type) => {
+      const apiURL = `${prefix}/_api/web/GetFolderByServerRelativeUrl('Shared%20Documents${path}')/${type}`;
+      const resp = await fetch(apiURL);
+      const xml = await resp.text();
+      const dp = new DOMParser();
+      const dom = dp.parseFromString(xml, 'text/xml');
+      const entries = [...dom.querySelectorAll('ServerRelativeUrl')];
+      return entries.map((e) => e.textContent.split('Shared Documents')[1]);
+    }
+
+    const getFiles = async (path) => await getDirEntries(path, 'Files');
+    const getFolders = async (path) => await getDirEntries(path, 'Folders');
+    const files = await getFiles(path);
+    files.every((file) => {
+      let cleanPath = '';
+      if (file.endsWith('.docx')) cleanPath = file.split('.')[0].substring(rootPath.length);
+      if (file.endsWith('.xlsx')) cleanPath = file.split('.')[0].substring(rootPath.length) + '.json';
+      if (file.endsWith('.pdf')) cleanPath = file.split('.')[0].substring(rootPath.length) + '.pdf';
+
+      cleanPath = cleanPath.toLowerCase();
+      cleanPath = cleanPath.replaceAll(' ', '-');
+      cleanPath = cleanPath.replaceAll('&', '-');
+      cleanPath = cleanPath.replaceAll('\'', '-');
+      cleanPath = cleanPath.replaceAll('--', '-');
+      cleanPath = cleanPath.replaceAll('--', '-');
+      cleanPath = cleanPath.replaceAll('--', '-');
+
+      if (cleanPath.trim() !== '') {
+        count += 1;
+        URLs.push(`${domainPrefix}${cleanPath}`);
+      }
+      return count < MAX_COUNT;
+
+    });
+
+    const folders = await getFolders(path);
+    for (let i = 0; i < folders.length; i += 1) {
+      const folder = folders[i];
+      await traverseFolder(folder);
+      console.log(`Collected ${URLs.length} urls`);
+    }
+  }
+  await traverseFolder(rootPath);
+  console.log(URLs.join('\n'));
+  for (let i = 0; i < URLs.length; i += 1) {
+    await postData(URLs[i], action);
+  }
+}
+
+traverseCurrentFolder();
