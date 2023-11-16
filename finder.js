2aa03af9d1468dcdc4592b12aaf81b8c9e360bf1
Satya Deep Maheshwari <satyam@adobe.com>
2023-11-16 18:42:38 +0530

    Add text search support in finder

--- finder.js
+++ finder.js
@@ -6,9 +6,6 @@ const resultCache = new NodeCache();
 
 async function fetchPage(url) {
     console.log(`Fetching ${url}`);
-    if (resultCache.has(url)) {
-        return resultCache.get(url);
-    }
     const browser = await chromium.launch();
     const page = await browser.newPage();
     await page.goto(url);
@@ -16,32 +13,30 @@ async function fetchPage(url) {
     await page.waitForTimeout(2000);
     const html = await page.content();
     await browser.close();
-    resultCache.set(url, { html, 'content-type': 'text/html' });
     return html;
 }
 export default async function find(srcUrl, selector) {
     console.log(`Finding ${selector} in ${srcUrl}`);
-    if (resultCache.has(srcUrl)) {
-        const result = resultCache.get(srcUrl);
-        if (result['content-type'] !== 'text/html') {
-            return false;
-        } else {
-            const $ = cheerio.load(result.html);
-            const elements = $(selector);
-            return elements && elements.length > 0;
-        }
-    } else {
+    if (!resultCache.has(srcUrl)) {
         const resp = await fetch(srcUrl);
         await resp.text();
         if (!resp.headers.get('content-type').trim().toLowerCase().includes('text/html')) {
             resultCache.set(srcUrl, {});
-            return false;
         } else {
             const html = await fetchPage(srcUrl);
-            debugger;
-            const $ = cheerio.load(html);
-            const elements = $(selector);
-            return elements && elements.length > 0;
+            resultCache.set(srcUrl, { html, 'content-type': 'text/html' });
         }
+    }
+
+    const result = resultCache.get(srcUrl);
+    if (result['content-type'] !== 'text/html') {
+        return false;
+    } else if (selector.startsWith('"') && selector.endsWith('"')) {
+        const searchText = selector.substring(1, selector.length - 1);
+        return result.html.includes(searchText);
+    } else {
+        const $ = cheerio.load(result.html);
+        const elements = $(selector);
+        return elements && elements.length > 0;
     }
 }
